5c8884575ff675942764b1db79512143b267ff64
13ace37 <github@xace.ch>
2026-09-24 01:39:32 +0200

    fixed runtime error and upgraded to .net10

--- src/SurfTimer.cs
+++ src/SurfTimer.cs
@@ -50,162 +50,165 @@ namespace SurfTimer;
 [MinimumApiVersion(337)]
 public partial class SurfTimer : BasePlugin
 {
-    private readonly ILogger<SurfTimer> _logger;
-    public static IServiceProvider ServiceProvider { get; private set; } = null!;
-    private readonly IDataAccessService? _dataService;
+	private readonly ILogger<SurfTimer> _logger;
+	public static IServiceProvider ServiceProvider { get; private set; } = null!;
+	private readonly IDataAccessService? _dataService;
 
-    // Inject ILogger and store IServiceProvider globally
-    public SurfTimer(ILogger<SurfTimer> logger, IServiceProvider serviceProvider)
-    {
-        _logger = logger;
-        ServiceProvider = serviceProvider;
-        _dataService = ServiceProvider.GetRequiredService<IDataAccessService>();
-    }
+	// Inject ILogger and store IServiceProvider globally
+	public SurfTimer(ILogger<SurfTimer> logger, IServiceProvider serviceProvider)
+	{
+		_logger = logger;
+		ServiceProvider = serviceProvider;
+		_dataService = ServiceProvider.GetRequiredService<IDataAccessService>();
+	}
 
-    // Metadata
-    public override string ModuleName => $"CS2 {Config.PluginName}";
-    public override string ModuleVersion => "DEV-1";
-    public override string ModuleDescription => "Official Surf Timer by the CS2 Surf Initiative.";
-    public override string ModuleAuthor => "The CS2 Surf Initiative - github.com/cs2surf";
+	// Metadata
+	public override string ModuleName => $"CS2 {Config.PluginName}";
+	public override string ModuleVersion => "DEV-1";
+	public override string ModuleDescription => "Official Surf Timer by the CS2 Surf Initiative.";
+	public override string ModuleAuthor => "The CS2 Surf Initiative - github.com/cs2surf";
 
-    // Globals
-    private readonly ConcurrentDictionary<int, Player> playerList = new();
-    internal static IDatabaseService DB { get; private set; } = null!;
-    public static Map CurrentMap { get; private set; } = null!;
+	// Globals
+	private readonly ConcurrentDictionary<int, Player> playerList = new();
+	internal static IDatabaseService DB { get; private set; } = null!;
+	public static Map CurrentMap { get; private set; } = null!;
 
-    /* ========== MAP START HOOKS ========== */
-    public void OnMapStart(string mapName)
-    {
-        // Initialise Map Object
-        if ((CurrentMap == null || CurrentMap.Name!.Equals(mapName)) && mapName.Contains("surf_"))
-        {
-            _logger.LogInformation(
-                "[{Prefix}] New map {MapName} started. Initializing Map object.....",
-                Config.PluginName,
-                mapName
-            );
+	/* ========== MAP START HOOKS ========== */
+	public void OnMapStart(string mapName)
+	{
+		// Initialise Map Object
+		if ((CurrentMap == null || CurrentMap.Name!.Equals(mapName)) && mapName.Contains("surf_"))
+		{
+			_logger.LogInformation(
+				"[{Prefix}] New map {MapName} started. Initializing Map object.....",
+				Config.PluginName,
+				mapName
+			);
 
-            Server.NextWorldUpdateAsync(async () => // NextWorldUpdate runs even during server hibernation
-            {
-                _logger.LogInformation(
-                    "{PluginLogo}\n"
-                        + "[CS2 Surf] {PluginName} v.{ModuleVersion} - loading map {MapName}.\n"
-                        + "[CS2 Surf] This software is licensed under the GNU Affero General Public License v3.0. See LICENSE for more information.\n"
-                        + "[CS2 Surf] ---> Source Code: https://github.com/CS2Surf/Timer\n"
-                        + "[CS2 Surf] ---> License Agreement: https://github.com/CS2Surf/Timer/blob/main/LICENSE\n",
-                    Config.PluginLogo,
-                    Config.PluginName,
-                    ModuleVersion,
-                    mapName
-                );
+			Server.NextWorldUpdateAsync(async () => // NextWorldUpdate runs even during server hibernation
+			{
+				_logger.LogInformation(
+					"{PluginLogo}\n"
+						+ "[CS2 Surf] {PluginName} v.{ModuleVersion} - loading map {MapName}.\n"
+						+ "[CS2 Surf] This software is licensed under the GNU Affero General Public License v3.0. See LICENSE for more information.\n"
+						+ "[CS2 Surf] ---> Source Code: https://github.com/CS2Surf/Timer\n"
+						+ "[CS2 Surf] ---> License Agreement: https://github.com/CS2Surf/Timer/blob/main/LICENSE\n",
+					Config.PluginLogo,
+					Config.PluginName,
+					ModuleVersion,
+					mapName
+				);
 
-                CurrentMap = new Map(mapName);
-                await CurrentMap.InitializeAsync();
-            });
-        }
-    }
+				CurrentMap = new Map(mapName);
+				await CurrentMap.InitializeAsync();
+			});
+		}
+	}
 
-    public void OnMapEnd()
-    {
-        _logger.LogInformation(
-            "[{Prefix}] Map ({MapName}) ended. Cleaning up resources...",
-            Config.PluginName,
-            CurrentMap.Name
-        );
+	public void OnMapEnd()
+	{
+		if (CurrentMap is not null)
+		{
+			_logger.LogInformation(
+				"[{Prefix}] Map ({MapName}) ended. Cleaning up resources...",
+				Config.PluginName,
+				CurrentMap.Name
+			);
+		}
 
-        // Clear/reset stuff here
-        CurrentMap = null!;
-        playerList.Clear();
-    }
+		// Clear/reset stuff here
+		CurrentMap = null!;
+		playerList.Clear();
+	}
 
-    [GameEventHandler]
-    public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
-    {
-        // Load cvars/other configs here
-        // Execute server_settings.cfg
+	[GameEventHandler]
+	public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+	{
+		// Load cvars/other configs here
+		// Execute server_settings.cfg
 
-        ConVarHelper.RemoveCheatFlagFromConVar("bot_stop");
-        ConVarHelper.RemoveCheatFlagFromConVar("bot_freeze");
-        ConVarHelper.RemoveCheatFlagFromConVar("bot_zombie");
+		ConVarHelper.RemoveCheatFlagFromConVar("bot_stop");
+		ConVarHelper.RemoveCheatFlagFromConVar("bot_freeze");
+		ConVarHelper.RemoveCheatFlagFromConVar("bot_zombie");
 
-        Server.ExecuteCommand("execifexists SurfTimer/server_settings.cfg");
-        _logger.LogTrace(
-            "[{Prefix}] Executed configuration: server_settings.cfg",
-            Config.PluginName
-        );
-        return HookResult.Continue;
-    }
+		Server.ExecuteCommand("execifexists SurfTimer/server_settings.cfg");
+		_logger.LogTrace(
+			"[{Prefix}] Executed configuration: server_settings.cfg",
+			Config.PluginName
+		);
+		return HookResult.Continue;
+	}
 
-    /* ========== PLUGIN LOAD ========== */
-    public override void Load(bool hotReload)
-    {
-        LocalizationService.Init(Localizer);
+	/* ========== PLUGIN LOAD ========== */
+	public override void Load(bool hotReload)
+	{
+		LocalizationService.Init(Localizer);
 
-        // === Dapper bootstrap (snake_case mapping + type handlers) + DB init ===
-        DapperBootstrapper.Init();
-        var connString = Config.MySql.GetConnectionString();
-        var factory = new MySqlConnectionStringFactory(connString);
-        DB = new DapperDatabaseService(factory);
+		// === Dapper bootstrap (snake_case mapping + type handlers) + DB init ===
+		DapperBootstrapper.Init();
+		var connString = Config.MySql.GetConnectionString();
+		var factory = new MySqlConnectionStringFactory(connString);
+		DB = new DapperDatabaseService(factory);
 
-        bool accessService = false;
+		bool accessService = false;
 
-        try
-        {
-            accessService = Task.Run(() => _dataService!.PingAccessService())
-                .GetAwaiter()
-                .GetResult();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(
-                ex,
-                "[{Prefix}] PingAccessService threw an exception.",
-                Config.PluginName
-            );
-        }
+		try
+		{
+			accessService = Task.Run(() => _dataService!.PingAccessService())
+				.GetAwaiter()
+				.GetResult();
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError(
+				ex,
+				"[{Prefix}] PingAccessService threw an exception.",
+				Config.PluginName
+			);
+		}
 
-        if (accessService)
-        {
-            _logger.LogInformation(
-                "[{Prefix}] {AccessService} connection established.",
-                Config.PluginName,
-                Config.Api.GetApiOnly() ? "API" : "DB"
-            );
-        }
-        else
-        {
-            _logger.LogCritical(
-                "[{Prefix}] Error connecting to the {AccessService}.",
-                Config.PluginName,
-                Config.Api.GetApiOnly() ? "API" : "DB"
-            );
+		if (accessService)
+		{
+			_logger.LogInformation(
+				"[{Prefix}] {AccessService} connection established.",
+				Config.PluginName,
+				Config.Api.GetApiOnly() ? "API" : "DB"
+			);
+		}
+		else
+		{
+			_logger.LogCritical(
+				"[{Prefix}] Error connecting to the {AccessService}.",
+				Config.PluginName,
+				Config.Api.GetApiOnly() ? "API" : "DB"
+			);
 
-            Exception exception = new(
-                $"[{Config.PluginName}] Error connecting to the {(Config.Api.GetApiOnly() ? "API" : "DB")}"
-            );
-            throw exception;
-        }
+			Exception exception = new(
+				$"[{Config.PluginName}] Error connecting to the {(Config.Api.GetApiOnly() ? "API" : "DB")}"
+			);
+			throw exception;
+		}
 
-        _logger.LogInformation(
-            """  
+		_logger.LogInformation(
+			"""  
                 {PluginLogo}  
                 [CS2 Surf] {PluginName} plugin loaded. Version: {ModuleVersion}
                 [CS2 Surf] This plugin is licensed under the GNU Affero General Public License v3.0. See LICENSE for more information. 
                 Source code: https://github.com/CS2Surf/Timer
             """,
-            Config.PluginLogo,
-            Config.PluginName,
-            ModuleVersion
-        );
+			Config.PluginLogo,
+			Config.PluginName,
+			ModuleVersion
+		);
 
-        // Map Start Hook
-        RegisterListener<Listeners.OnMapStart>(OnMapStart);
-        // Map End Hook
-        RegisterListener<Listeners.OnMapEnd>(OnMapEnd);
-        // Tick listener
-        RegisterListener<Listeners.OnTick>(OnTick);
+		// Map Start Hook
+		RegisterListener<Listeners.OnMapStart>(OnMapStart);
+		// Map End Hook
+		RegisterListener<Listeners.OnMapEnd>(OnMapEnd);
+		// Tick listener
+		RegisterListener<Listeners.OnTick>(OnTick);
 
-        HookEntityOutput("trigger_multiple", "OnStartTouch", OnTriggerStartTouch);
-        HookEntityOutput("trigger_multiple", "OnEndTouch", OnTriggerEndTouch);
-    }
+		HookEntityOutput("trigger_multiple", "OnStartTouch", OnTriggerStartTouch);
+		HookEntityOutput("trigger_multiple", "OnEndTouch", OnTriggerEndTouch);
+	}
 }
